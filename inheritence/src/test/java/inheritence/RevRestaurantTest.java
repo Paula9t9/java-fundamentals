264e0175e85f3b517b5089bdeb53ddaaae2c3f1d
Paula Thomas <paula9t9@gmail.com>
2019-06-05 11:40:04 -0700

    RevRestaurant passes tests

--- inheritence/src/test/java/inheritence/RevRestaurantTest.java
+++ inheritence/src/test/java/inheritence/RevRestaurantTest.java
@@ -1,47 +1,57 @@
-//package inheritence;
-//
-//import org.junit.Before;
-//import org.junit.Test;
-//
-//import static org.junit.Assert.*;
-//
-//public class RevRestaurantTest {
-//
-//    Restaurant testRestaurant;
-//    Review testReview;
-//
-//    @Before
-//    public void initializeVariables(){
-//        testRestaurant = initializeRestaurant();
-//        testReview = initializeReview(testRestaurant);
-//    }
-//
-//    @Test
-//    public void testReviewConstructor(){
-//        assertEquals("Author should be Gordon Ramsay",
-//                "Gordon Ramsay", testReview.getAuthor());
-//        assertEquals("Body should read: 'This is awful.'",
-//                "This is awful.", testReview.getBody());
-//        assertEquals("Stars should be 0", 0, testReview.getStars());
-//        assertEquals("Should have the correct restaurant object",
-//                testRestaurant, testReview.getRestaurant());
-//    }
-//
-//
-//    @Test
-//    public void toString1() {
-//    }
-//
-//    @Test
-//    public void equals1() {
-//    }
-//
-//    private Restaurant initializeRestaurant(){
-//        return new Restaurant("Cash Brewery", 2);
-//    }
-//
-//    private Review initializeReview(Restaurant restaurant){
-//        return new Review( restaurant,
-//                "Gordon Ramsay", "This is awful.", 0);
-//    }
-//}
+package inheritence;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RevRestaurantTest {
+
+    Restaurant testRestaurant;
+    RevRestaurant testReview;
+
+    @Before
+    public void initializeVariables(){
+        testRestaurant = initializeRestaurant();
+        testReview = initializeReview(testRestaurant);
+    }
+
+    @Test
+    public void testReviewConstructor(){
+        assertEquals("Author should be Gordon Ramsay",
+                "Gordon Ramsay", testReview.getAuthor());
+        assertEquals("Body should read: 'This is awful.'",
+                "This is awful.", testReview.getBody());
+        assertEquals("Stars should be 0", 0, testReview.getStars());
+        assertEquals("Should have the correct restaurant object",
+                testRestaurant, testReview.getRestaurant());
+    }
+
+
+    @Test
+    public void tesRevRestaurantToString1() {
+        String expectedString = "Review for restaurant Cash Brewery by author: Gordon Ramsay, stars: 0, body: This is" +
+                " awful.";
+
+        assertEquals("Should return toString for appropriate Review",
+                expectedString, testReview.toString());
+    }
+
+    @Test
+    public void testEquals(){
+        RevRestaurant sneakyCloneTestReview = initializeReview(testRestaurant);
+
+        assertEquals("Review should equal itself", testReview, testReview);
+        assertEquals("Restaurant should equal restaurant with same values",
+                testReview, sneakyCloneTestReview);
+    }
+
+    private Restaurant initializeRestaurant(){
+        return new Restaurant("Cash Brewery", 2);
+    }
+
+    private RevRestaurant initializeReview(Restaurant restaurant){
+        return new RevRestaurant( restaurant,
+                "Gordon Ramsay", "This is awful.", 0);
+    }
+}
